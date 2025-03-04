8a0d8659b9a31be907ae45218bc77b27addfe546
Vivek <vivekanandareddy7677@gmail.com>
2025-03-03 21:53:34 -0600

    Setting up vault pipeline

--- module/main.tf
+++ module/main.tf
@@ -61,7 +61,7 @@ resource "aws_iam_role_policy" "policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action   = var.policy_resource_list
+        Action   = concat(var.dummy_policy,var.policy_resource_list)
         Effect   = "Allow"
         Resource = "*"
       },
